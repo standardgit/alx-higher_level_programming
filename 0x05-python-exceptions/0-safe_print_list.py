f220679b2df441e22c0225129407e00b5890eb84
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-19 00:36:10 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{:d}".format(my_list[i]), end="")
             num = num + 1
-    except:
+    except (ValueError, IndexError):
         None
     print()
     return num
