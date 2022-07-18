9fa5495fd7fdf4530c8ec7feeadb21efb4b6f2b7
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-19 00:40:42 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num = num + 1
-        except ValueError:
+        except (ValueError, IndexError):
             None
     print()
 
-    return 
+    return num
