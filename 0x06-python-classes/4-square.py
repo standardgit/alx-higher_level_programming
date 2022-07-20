b853ae2f0da4d393053888a1988bdac54ed6b0a9
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-20 01:50:09 +0100

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A Square Class with private attribute
+ and validations and method with a setter"""
+
+
+class Square:
+    """A class that have a private attribute size and
+    calculate the area and also change value with setter"""
+    def __init__(self, size=0):
+        try:
+            if size >= 0:
+                self.__size = size
+        except ValueError:
+            raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        try:
+            if value >= 0:
+                self.__size = value
+        except ValueError:
+            raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
