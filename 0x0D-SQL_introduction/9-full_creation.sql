dc08676ca628944b95907e20338ccf0c0be12fd6
standardgit <standardblaise@gmail.com>
2022-09-06 07:56:34 -0700

    Creating second table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creating full database table
+-- Adding multiple rows
+CREATE TABLE second_table (
+	id (int),
+	name (varcharr(256))
+	score (int),);
+INSERT INTO second_table (id, name, score) VALUES(
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8));
