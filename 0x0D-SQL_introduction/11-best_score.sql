c165c64b949a9f13252765b42622c4fd11e99e2b
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-09-07 05:26:40 +0100

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Selecting the best score
 -- Listing by score
-SELECT * FROM second_table WHERE score >= 10 ORDER BY DESC
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
