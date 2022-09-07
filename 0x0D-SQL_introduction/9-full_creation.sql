94399ce06560d020b2ad0df72ed288f258c827a6
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-09-07 04:42:49 +0100

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,9 +3,9 @@
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
-	score INT); ENGINE = INNODB
-INSERT INTO second_table VALUES(
+	score INT) ENGINE = INNODB
+INSERT INTO second_table VALUES
 	(1, 'John', 10),
 	(2, 'Alex', 3),
 	(3, 'Bob', 14),
-	(4, 'George', 8));
+	(4, 'George', 8);
