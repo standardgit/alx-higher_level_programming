8a517083db1694a5d42b1467ef91d511a85dd3ff
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-09-08 05:40:18 +0100

    Create 7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creating a database and table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
+) ENGINE=INNODB;
