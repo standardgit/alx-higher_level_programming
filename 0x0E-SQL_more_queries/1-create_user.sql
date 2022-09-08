e42a01edd6dc36c18a99454b0e6ef7f49c49d9c3
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-09-08 05:14:28 +0100

    Create 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creating a user
+-- Granting all priviledges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
