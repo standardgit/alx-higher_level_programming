63918bb28895685551c94ce7127514ee9ba668e7
standardgit <standardblaise@gmail.com>
2022-10-04 03:23:41 +0000

    alzz

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const process = require('process');
-const argv = process.argv
+const argv = process.argv;
 
 if (argv.length <= 2) {
-console.log('No argument')
-} else if (argv.length == 3) {
-console.log('Argument found')
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else if (argv.length >= 3) {
-console.log('Arguments found')};
+  console.log('Arguments found');
+}
