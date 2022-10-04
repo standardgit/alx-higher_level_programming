8cd124ffa63ad0ce42e98035781468aef76a7ce9
standardgit <standardblaise@gmail.com>
2022-10-04 12:17:33 +0000

    change it push

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,17 @@
-#!/usr/bin/node
+i#!/usr/bin/node
 
 const process = require('process');
 const argv = process.argv;
 
-const fact = (num) => {
+const factorial = function(num) {
   if (num === 1) {
     return 1;
   }
-  return num * fact(num - 1);
+  return num * factorial(num - 1);
 };
 
 if (argv[2] <= 0 || argv[2] >= 0) {
-  console.log(fact(Number(argv[2])));
+  console.log(factorial(Number(argv[2])));
 } else {
   console.log(1);
 }
