dcee77c2549fe7d41732e8735626b75a1ddcbaf8
standardgit <standardblaise@gmail.com>
2022-10-04 03:27:52 +0000

    nope

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const process = require('process')
-const argv = process.argv
+const process = require('process');
+const argv = process.argv;
 
 if (argv[2] <= 0 || argv[2] >= 0) {
-console.log(`My number: ${parseInt(argv[2])}`)
+  console.log(`My number: ${parseInt(argv[2])}`);
 } else {
-console.log('Not a number')};
+  console.log('Not a number');
+}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,4 +3,5 @@
 const argv = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < argv.length; i++) {
-  console.log(argv[i])};
+  console.log(argv[i]);
+}
