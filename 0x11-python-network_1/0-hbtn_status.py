732af0a1ff2a87315d7e8513381a6ec5d89710e9
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-26 00:01:15 +0100

    request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/bin/usr/python3
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+from urllib.request import urlopen
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
 - utf8 content: {}".format(type(html), html, html.decode("utf-8")))
