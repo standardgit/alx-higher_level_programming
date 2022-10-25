6c7687bf1f4e30a00fdf0efc1bb4a5db6251dd9a
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-25 23:58:49 +0100

    space

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/bin/usr/python3
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
-
 import urllib.request
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
