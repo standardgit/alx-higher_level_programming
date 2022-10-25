84e48b997530dbc425a8269e6ba619f0c08cda23
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-26 00:40:07 +0100

    request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib.request import urlopen
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    with urlopen(url) as response:
+        header_request_id = response.header.get("X-Request-Id")
+        print(header_request_id)
