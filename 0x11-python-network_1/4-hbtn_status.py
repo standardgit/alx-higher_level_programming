822b789efa424f8bfcb644a4396ae6c8d0b410ba
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-26 01:59:15 +0100

    no tab

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,5 +4,5 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\
 ".format(type(r.text), r.text))
