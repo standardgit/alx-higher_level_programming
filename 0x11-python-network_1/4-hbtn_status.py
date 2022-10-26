782e0ce5c9060a528f9c82b831f6a92fa95f7d71
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-26 01:57:23 +0100

    requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """A Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
-from sys import argv
+import requests
 
 if __name__ == "__main__":
-    r = request.get('https://alx-intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
 ".format(type(r.text), r.text))
