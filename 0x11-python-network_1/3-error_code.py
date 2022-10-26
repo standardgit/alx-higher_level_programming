6c98759ad3e073f2d658329b539bbbd7a479d216
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-26 01:22:43 +0100

    Error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.error
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with  urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode("utf-8"))
+    except urllib.error.URLError as e:
+        print("Error code:" e.code)
+    
