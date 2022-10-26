ab35657700039abccc09c7df6dece8a735638abf
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-26 01:25:59 +0100

    pycode

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,9 +7,8 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     try:
-        with  urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:
             body = response.read()
             print(body.decode("utf-8"))
     except urllib.error.URLError as e:
-        print("Error code:" e.code)
-    
+       print(f"Error code: {e.code}")
