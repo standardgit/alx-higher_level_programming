a8c5ccd029b1660bd511fe87e441e397ee49d9a7
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-10-26 01:02:57 +0100

    decode

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
-    print(the_page)
+    print(the_page.decode("utf-8"))
